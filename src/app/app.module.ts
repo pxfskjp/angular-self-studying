5d73d0a2e8feb4d053b528d451f92c970a5126e6
topdeveloper0729 <69484492+topdeveloper0729@users.noreply.github.com>
2021-03-31 13:50:06 -0300

    Generate Product list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './pages/top-bar/top-bar.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'product-list', component: ProductListComponent },
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppModule { }
---
+++ src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent {
+  products = products;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
