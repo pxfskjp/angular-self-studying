308269c72ea38b14dd8608337268e15ce1887f94
topdeveloper0729 <69484492+topdeveloper0729@users.noreply.github.com>
2021-04-02 01:07:15 -0300

    Blank Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './pages/top-bar/top-bar.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
-
-const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'product-list', component: ProductListComponent },
-]
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent
   ],
   bootstrap: [
     AppComponent
