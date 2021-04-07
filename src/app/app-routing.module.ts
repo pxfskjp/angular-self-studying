c5858314e9dfebab2c98d9350955bfb74aa724ee
topdeveloper0729 <69484492+topdeveloper0729@users.noreply.github.com>
2021-04-07 02:01:41 -0300

    Generate Hero list, Crisis lists, Page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [
-  { path: '', component: AppComponent },
-];
+const appRoute: Routes = [
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  { path: 'crisis-list', component: CrisisListComponent },
+  {path: 'hero-list', component: HeroListComponent},
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   imports: [
     RouterModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(
+      appRoute,
+      {enableTracing: true}  
+    )
   ],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoute: Routes = [
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  { path: 'crisis-list', component: CrisisListComponent },
+  {path: 'hero-list', component: HeroListComponent},
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    RouterModule.forRoot(
+      appRoute,
+      {enableTracing: true}
+    )
   ],
   declarations: [
     AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent,
   ],
   bootstrap: [
     AppComponent
-  ],
-  exports: [
-    RouterModule
   ]
 })
 export class AppModule { }
